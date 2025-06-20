e75705431b218822d207adff3f167dcdabe6699d
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-06-20 01:39:23 -0300

    feat: atualizando caminho de imagem

--- LP-INTERACTPLAY/src/components/Sections/Home/index.tsx
+++ LP-INTERACTPLAY/src/components/Sections/Home/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.scss";
-import SvgSeta from "../../../../public/media/icons/tail-right.svg";
-import Hero from "../../../../public/media/images/hero.png";
+import SvgSeta from "/media/icons/tail-right.svg";
+import Hero from "/media/images/hero.png";
 import Features from "../../Features";
 import AnimatedCounter from "../../AnimatedCounter";
 import { metricsData } from "../../../data/metrics";
