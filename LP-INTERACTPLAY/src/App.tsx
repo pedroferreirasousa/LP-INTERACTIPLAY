3ae8737fe9f7141b2d7994923da4895c9819cae7
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-06-20 01:26:55 -0300

    feat: renderizando hihlights no app.tsx

--- LP-INTERACTPLAY/src/App.tsx
+++ LP-INTERACTPLAY/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Highlights from "./components/Sections/Highlights";
 import Home from "./components/Sections/Home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Highlights />
     </>
   );
 }
