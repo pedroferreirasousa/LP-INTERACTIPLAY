67d673983d1f41d0ae80217771c558ea1578501b
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-06-20 21:32:21 -0300

    feat: criando e estilizando section contact

---
+++ LP-INTERACTPLAY/src/components/Sections/Contact/index.tsx
@@ -0,0 +1,21 @@
+import FormCadastro from "../../Form";
+import styles from "./styles.module.scss";
+
+
+const Contact = () => {
+    return (
+        <div className={styles.section}>
+            <div className={styles.left__container}>
+            <h2>Transforme sua Edição de Vídeo com a InteractiPlay!</h2>
+            <span>Preencha o formulário para receber orientação personalizada sobre como a InteractPlay pode revolucionar sua edição de vídeo!</span>
+            </div>
+
+            <div className={styles.right__container}>
+                <FormCadastro />
+            </div>
+
+        </div>
+    )
+};
+
+export default Contact;
