4378745eaab4426ca0037fafa4526b09c1724dca
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-06-23 16:32:52 -0300

    feat: adicionando novos comentarios no testimonial

--- LP-INTERACTPLAY/src/data/testimony.ts
+++ LP-INTERACTPLAY/src/data/testimony.ts
@@ -15,19 +15,19 @@ export const testimonials: Testimonial[] = [
   {
     id: "2",
     image: "/media/images/testimonyprofile2.png",
-    name: "Pedro Ferreira",
+    name: "Pedro Henrique",
     text: "Recursos avançados e facilidade. Aumentou minhas taxas de conversão. Solução completa para marketing digital!",
   },
   {
     id: "3",
-    image: "/media/images/testimonyprofile2.png",
-    name: "Pedro Ferreira",
-    text: "Recursos avançados e facilidade. Aumentou minhas taxas de conversão. Solução completa para marketing digital!",
+    image: "/media/images/testimonyprofile3.png",
+    name: "Vitor Ferreira",
+    text: "A InteractPlay revolucionou a forma como crio VSLs! Os botões de ação interativos e o SmartPlay são um diferencial enorme para minhas vendas. Edição profissional e resultados impactantes em uma única plataforma!",
   },
   {
     id: "4",
-    image: "/media/images/testimonyprofile2.png",
-    name: "Pedro Ferreira",
-    text: "Recursos avançados e facilidade. Aumentou minhas taxas de conversão. Solução completa para marketing digital!",
+    image: "/media/images/testimonyprofile4.png",
+    name: "Fernando Augusto",
+    text: "Finalmente uma plataforma completa para edição de vídeos! Com a InteractPlay, consigo criar conteúdos dinâmicos com o Flow e impulsionar minhas conversões de forma eficiente. A interface é intuitiva e os recursos são poderosos!",
   },
 ];
